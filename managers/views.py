8128a07d0c1d4c22467dacf1878d1f2df4ab23b5
Jakub Kotlorz <jkotlorz@protonmail.com>
2018-09-16 18:03:18 +0200

    Basic views added.

--- managers/views.py
+++ managers/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Manager, Country
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    countries_list = Country.objects.order_by('id')
+    context = { 'countries_list': countries_list, }
+    return render(request, 'managers/index.html', context)
+
+def country(request, country_id):
+    country = get_object_or_404(Country, pk=country_id)
+    managers = Manager.objects.filter(country=country_id)
+    context = { 'country': country, 'managers': managers}
+    return render(request, 'managers/country.html', context) 
+
+def profile(request, manager_id):
+    person = get_object_or_404(Manager, pk=manager_id)
+    country = get_object_or_404(Country, pk=person.country_id)
+    context = { 'person': person, 'country': country }
+    return render(request, 'managers/profile.html', context)
