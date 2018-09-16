3753a0b890e711d48af331617a052a2293d4e85d
Jakub Kotlorz <jkotlorz@protonmail.com>
2018-09-16 18:16:51 +0200

    Namespacing url templates.

--- managers/urls.py
+++ managers/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'managers'
 urlpatterns = [
     path('', views.index, name='index'),
     path('country/<int:country_id>/', views.country, name='country'),
