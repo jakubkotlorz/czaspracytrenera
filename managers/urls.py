963c833eff9ebfb84b7a0d713f6d3a06f4540959
tenosoft <jkotlorz@protonmail.com>
2018-09-21 18:17:06 +0200

    URLs fixed so no app name is required and additional 'm/' path for manager's profile added.

--- managers/urls.py
+++ managers/urls.py
@@ -6,5 +6,5 @@ app_name = 'managers'
 urlpatterns = [
     path('', views.index, name='index'),
     path('country/<int:country_id>/', views.country, name='country'),
-    path('<int:manager_id>/', views.profile, name='profile'),
+    path('m/<int:manager_id>/', views.profile, name='profile'),
 ]
