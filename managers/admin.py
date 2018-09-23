bcc74d9d093c56ca6354fe36aa71aca859fa4ddc
tenosoft <jkotlorz@protonmail.com>
2018-09-23 11:21:19 +0200

    Admin classed added. Flags updated. __str__ functions added to models.

--- managers/admin.py
+++ managers/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Country, City, Manager, Season, Team, TeamSeason, Employment
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name_pl', 'name_en', 'icon_name')
+admin.site.register(Country, CountryAdmin)
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name_pl', 'country')
+admin.site.register(City, CityAdmin)
