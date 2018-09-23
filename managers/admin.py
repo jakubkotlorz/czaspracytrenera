bed4dac47ba0c3c941672349586ea5bbf87db810
Jakub <kubakotlorz@gmail.com>
2018-09-23 12:36:04 +0200

    Photo added to Manager model. Also admin panel for Manager added.

--- managers/admin.py
+++ managers/admin.py
@@ -11,3 +11,12 @@ admin.site.register(Country, CountryAdmin)
 class CityAdmin(admin.ModelAdmin):
     list_display = ('name_pl', 'country')
 admin.site.register(City, CityAdmin)
+
+
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ('name_first', 'name_last', 'country', 'date_birth', 'city_birth', 'slug', 'photo')
+    list_filter = ('country', )
+    search_fields = ('name_first', 'name_last')
+    prepopulated_fields = { 'slug': ('name_first', 'name_last', )}
+    ordering = ['name_last', 'country']
+admin.site.register(Manager, ManagerAdmin)
