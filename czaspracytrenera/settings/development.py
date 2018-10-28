db843b3a8ed14d5b67afc41686b9af16cfcb95e3
tenosoft <jkotlorz@protonmail.com>
2018-10-28 20:33:19 +0100

    Split settings to development and production

---
+++ czaspracytrenera/settings/development.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATIC_URL = '/static/'
---
+++ czaspracytrenera/settings/production.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["czaspracytrenera.pl"]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+ENV_PATH = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(ENV_PATH, '../public/static/') 
+MEDIA_ROOT = os.path.join(ENV_PATH, '../public/media/') 
