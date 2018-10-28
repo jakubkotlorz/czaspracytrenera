6bb753f6aa63bd76070596d0a789942ceb416005
tenosoft <jkotlorz@protonmail.com>
2018-10-28 20:33:19 +0100

    Split settings to development and production

--- czaspracytrenera/settings/base.py
+++ czaspracytrenera/settings/base.py
@@ -22,11 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '^h!r^&rz8u@u=jpf+=8rtyg1%!_crs)#rr(f&%9m_wte0%cli%'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -113,9 +108,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
