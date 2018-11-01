7e17aab885c266eba264e7abc02016463ea069a2
tenosoft <jkotlorz@protonmail.com>
2018-11-01 23:31:59 +0100

    Statistics script saves csv file now.

--- db_stats.py
+++ db_stats.py
@@ -7,22 +7,27 @@ import csv
 
 sqlite_file = 'db.sqlite3'
 app_name = 'managers'
-table_names = ['city', 'country', 'employment', 'externallink', 'manager', 'season', 'team', 'teamseason']
+column_names = ['city', 'country', 'employment', 'externallink', 'manager', 'season', 'team', 'teamseason']
 out_file = 'statistics.csv'
 
 def main():
     """ Main program """
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
-    data = [str(datetime.datetime.now().date())]
-    for col in table_names:
-        query = f"SELECT COUNT(*) FROM {app_name}_{col}"
+
+    values = dict()
+    values['date'] = str(datetime.datetime.now().date())
+    columns = [f"{app_name}_{col}" for col in column_names]
+    for col in columns:
+        query = f"SELECT COUNT(*) FROM {col}"
         c.execute(query)
         count = c.fetchone()[0]
-        item = f"{app_name}_{col}:{count}"
-        data.append(item)
-    line = ';'.join(data)
-    print(line)
+        values[col] = count
+
+    with open(out_file, 'a') as file:
+        writer = csv.DictWriter(file, values.keys())
+        writer.writerow(values)
+        file.close()
     return 0
 
 if __name__ == "__main__":
