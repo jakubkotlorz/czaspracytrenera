338913890a47a088b3614140d7a359d220a160c3
Jakub <kubakotlorz@gmail.com>
2019-08-23 19:31:04 +0200

    Try to fix server bug with accessing media by URL

--- managers/files_handling.py
+++ managers/files_handling.py
@@ -5,6 +5,6 @@ from os.path import join
 
 def upload_photo(file):
     photos_location = join(settings.MEDIA_ROOT, 'photos')
-    fs = FileSystemStorage(location=photos_location, base_url=photos_location)
+    fs = FileSystemStorage(location=photos_location, base_url='media/photos/')
     filename = fs.save(file.name, file)
-    return fs.url(filename)
+    return filename
